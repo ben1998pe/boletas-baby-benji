897d52271ef6f56db1644eefde028c77c09413d4
Benjamin Oscco Arias <72709113+ben1998pe@users.noreply.github.com>
2025-06-04 17:44:49 +0000

    fix: agregar import os para evitar error en Cloudinary

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import sqlite3
 from datetime import datetime
+import os
 import cloudinary
 import cloudinary.uploader
 
